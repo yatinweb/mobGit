49d8acf9000f991d4040f52c4d533c90b239334a
Yatin Patel <yatinweb@gmail.com>
2018-04-05 19:17:32 +0530

    - Added config for ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -4,41 +4,51 @@
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-junit-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
-    ],
-    client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-      captureConsole: Boolean(process.env.KARMA_ENABLE_CONSOLE)
-    },
-    junitReporter: {
-      outputDir: 'reports/junit/',
-      outputFile: 'TESTS-xunit.xml',
-      useBrowserName: false,
-      suite: '' // Will become the package name attribute in xml testsuite element
-    },
-    coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly', 'text-summary'],
-      dir: './reports/coverage',
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['progress', 'junit'],
-    port: 9876,
-    colors: true,
-    // Level of logging, can be: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: true
-  });
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular/cli'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-junit-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular/cli/plugins/karma')
+        ],
+        client: {
+            clearContext: false, // leave Jasmine Spec Runner output visible in browser
+            captureConsole: Boolean(process.env.KARMA_ENABLE_CONSOLE)
+        },
+        junitReporter: {
+            outputDir: 'reports/junit/',
+            outputFile: 'TESTS-xunit.xml',
+            useBrowserName: false,
+            suite: '' // Will become the package name attribute in xml testsuite element
+        },
+        coverageIstanbulReporter: {
+            reports: ['html', 'lcovonly', 'text-summary'],
+            dir: './reports/coverage',
+            fixWebpackSourcePaths: true
+        },
+        angularCli: {
+            environment: 'dev'
+        },
+        reporters: ['progress', 'junit'],
+        port: 9876,
+        colors: true,
+        // Level of logging, can be: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['ChromeHeadlessNoSandbox'],
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: [
+                    '--no-sandbox', // required to run without privileges in docker
+                    '--user-data-dir=/tmp/chrome-test-profile',
+                    '--disable-web-security'
+                ]
+            }
+        },
+        singleRun: true
+    });
 };
