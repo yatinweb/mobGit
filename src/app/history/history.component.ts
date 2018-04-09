e7ff6a4703b9914323b37d7c64399f4cd4753a89
Yatin Patel <yatinweb@gmail.com>
2018-04-09 11:06:18 +0530

    - Updated Sorting Code

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -15,10 +15,17 @@ export class HistoryComponent implements OnInit {
   _isFavoriteList: any[];
   _isHisrotyList: any[];
   pushPage: any;
-  resultSort = '';
+  resultSort = 'time';
 
   constructor() {
     this.pushPage = ProfileComponent;
+    this.getItems();
+  }
+
+  ngOnInit() {
+  }
+
+  getItems() {
     const isFavoriteList = localStorage.getItem(isFavoriteListKey);
     if (isFavoriteList) {
       this._isFavoriteList = JSON.parse(isFavoriteList);
@@ -28,25 +35,31 @@ export class HistoryComponent implements OnInit {
     const isHistoryList = localStorage.getItem(isHistoryListKey);
     if (isHistoryList) {
       this._isHisrotyList = JSON.parse(isHistoryList);
-      this._isHisrotyList = this._isHisrotyList.slice().reverse();
+      this.changeSort('time');
     } else {
       this._isHisrotyList = [];
     }
-  }
-
-  ngOnInit() {
   }
 
   removeHistory() {
     this._isHisrotyList = [];
     localStorage.removeItem(isHistoryListKey);
   }
+
+  ionViewWillEnter() {
+   this.getItems();
+  }
+
   checkForfavorite(login: string) {
     return _.includes(this._isFavoriteList, login);
   }
 
   changeSort(ev: any) {
-    // TODO: Change order by name
+    if (ev === 'time') {
+      this._isHisrotyList = this._isHisrotyList.slice().reverse();
+    } else if (ev === 'name') {
+      this._isHisrotyList = _.orderBy(this._isHisrotyList, ['login'], ['asc']);
+     }
   }
 
 }
